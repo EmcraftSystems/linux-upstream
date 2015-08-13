fc808022feb3a49007bb29bdc0572a8f8963cbb6
Sergei Miroshnichenko <sergeimir@emcraft.com>
2015-08-13 07:35:48 +0300

    RT #110056 Set proper ECC layout for Vibrid NAND
    Merged from #102292

--- drivers/mtd/nand/vf610_nfc.c
+++ drivers/mtd/nand/vf610_nfc.c
@@ -165,15 +165,15 @@ struct vf610_nfc {
 
 static struct nand_ecclayout vf610_nfc_ecc45 = {
 	.eccbytes = 45,
-	.eccpos = {19, 20, 21, 22, 23,
+	.eccpos = {16, 17, 20, 21, 22, 23,
 		   24, 25, 26, 27, 28, 29, 30, 31,
 		   32, 33, 34, 35, 36, 37, 38, 39,
 		   40, 41, 42, 43, 44, 45, 46, 47,
 		   48, 49, 50, 51, 52, 53, 54, 55,
 		   56, 57, 58, 59, 60, 61, 62, 63},
 	.oobfree = {
-		{.offset = 2,
-		 .length = 17} }
+		{.offset = 4,
+		 .length = 8}}
 };
 
 static struct nand_ecclayout vf610_nfc_ecc60 = {
