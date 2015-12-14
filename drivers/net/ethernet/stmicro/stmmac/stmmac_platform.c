5e271d196c3ec97b59e5353c5ef826f5d701b595
Phil Reid <preid@electromag.com.au>
2015-12-14 11:31:59 +0800

    stmmac: create of compatible mdio bus for stmmac driver
    The DSA driver needs to be passed a reference to an mdio bus. Typically
    the mac is configured to use a fixed link but the mdio bus still needs
    to be registered so that it con configure the switch.
    This patch follows the same process as the altera tse ethernet driver for
    creation of the mdio bus.
    
    Acked-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Phil Reid <preid@electromag.com.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -166,7 +166,7 @@ static int stmmac_probe_config_dt(struct platform_device *pdev,
 	if (of_property_read_u32(np, "snps,phy-addr", &plat->phy_addr) == 0)
 		dev_warn(&pdev->dev, "snps,phy-addr property is deprecated\n");
 
-	if (plat->phy_node || plat->phy_bus_name)
+	if ((plat->phy_node && !of_phy_is_fixed_link(np)) || plat->phy_bus_name)
 		plat->mdio_bus_data = NULL;
 	else
 		plat->mdio_bus_data =
