e2c9d55c09aadf37a3c1fb14dfca718ad56c4596
Sergei Miroshnichenko <sergeimir@emcraft.com>
2016-01-22 19:14:43 +0300

    Revert "RM#197 i2c: Handle IRQ in client, not in adapter"
    This reverts commit 1cdb3fd37d28bcffcc6f44e01896a4ea2477d994.

--- drivers/i2c/busses/i2c-imx.c
+++ drivers/i2c/busses/i2c-imx.c
@@ -1236,8 +1236,12 @@ static int i2c_imx_suspend(struct device *dev)
 	struct platform_device *pdev = to_platform_device(dev);
 	struct imx_i2c_struct *i2c_imx = platform_get_drvdata(pdev);
 
-	if (!device_may_wakeup(&i2c_imx->adapter.dev))
+	if (device_may_wakeup(&i2c_imx->adapter.dev)) {
+		enable_irq_wake(i2c_imx->irq);
+	} else {
+		disable_irq(i2c_imx->irq);
 		clk_disable_unprepare(i2c_imx->clk);
+	}
 
 	return 0;
 }
@@ -1247,8 +1251,12 @@ static int i2c_imx_resume(struct device *dev)
 	struct platform_device *pdev = to_platform_device(dev);
 	struct imx_i2c_struct *i2c_imx = platform_get_drvdata(pdev);
 
-	if (!device_may_wakeup(&i2c_imx->adapter.dev))
+	if (device_may_wakeup(&i2c_imx->adapter.dev)) {
+		disable_irq_wake(i2c_imx->irq);
+	} else {
 		clk_prepare_enable(i2c_imx->clk);
+		enable_irq(i2c_imx->irq);
+	}
 
 	return 0;
 }
