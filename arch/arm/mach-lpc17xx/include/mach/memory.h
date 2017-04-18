0a98fd17c39a456304855548b690f3ca209bae7e
Sergei Miroshnichenko <sergeimir@emcraft.com>
2017-04-18 15:56:58 +0300

    RM#1468 ARM: lpc17xx: Add mach/memory.h for vector table copying to SRAM

---
+++ arch/arm/mach-lpc17xx/include/mach/memory.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2017 Emcraft Systems
+ * Sergei Miroshnichenko <sergeimir@emcraft.com>
+ *
+ * License terms:  GNU General Public License (GPL), version 2
+ */
+#ifndef __ASM_ARCH_MEMORY_H
+#define __ASM_ARCH_MEMORY_H
+
+#define SRAM_PHYS_OFFSET	0x20000000
+
+#endif
